dc150468fa2c16d9680b5d7da93d5bf7466baf57
Luca Erbetta <lucaerbetta105@gmail.com>
2019-05-25 00:52:14 +0200

    Increased main stack size

--- miosix/config/arch/cortexM4_stm32f4/stm32f429zi_skyward_death_stack/board_settings.h
+++ miosix/config/arch/cortexM4_stm32f4/stm32f429zi_skyward_death_stack/board_settings.h
@@ -46,7 +46,7 @@ namespace miosix {
 /// Size of stack for main().
 /// The C standard library is stack-heavy (iprintf requires 1KB) but the
 /// STM32F407VG only has 192KB of RAM so there is room for a big 4K stack.
-const unsigned int MAIN_STACK_SIZE=4*1024;
+const unsigned int MAIN_STACK_SIZE=16*1024;
 
 /// Frequency of tick (in Hz). The frequency of the STM32F100RB timer in the
 /// stm32vldiscovery board can be divided by 1000. This allows to use a 1KHz
