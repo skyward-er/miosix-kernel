ef8bc75d52a62c7a021052b0ca2b776e68b7e8b4
Damiano Amatruda <damiano.amatruda@skywarder.eu>
2021-11-08 12:53:11 +0100

    Create CMake equivalent of miosix/Makefile and CMake file for boards
    * Miosix is configured for every board.
    
    * File 'boards.cmake', containing list 'BOARDS' of all supported boards, is added.

---
+++ miosix/config/boards.cmake
@@ -0,0 +1,68 @@
+# Copyright (c) 2021 Skyward Experimental Rocketry
+# Author: Damiano Amatruda
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+set(BOARDS
+    lpc2138_miosix_board
+    stm32f103ze_stm3210e-eval
+    stm32f103ve_mp3v2
+    stm32f100rb_stm32vldiscovery
+    stm32f103ve_strive_mini
+    stm32f103ze_redbull_v2
+    stm32f407vg_stm32f4discovery
+    stm32f207ig_stm3220g-eval
+    stm32f207zg_ethboard_v2
+    stm32f207ze_als_camboard
+    stm32f205rg_sony-newman
+    stm32l151_als_mainboard
+    stm32f407vg_bitsboard
+    stm32f429zi_stm32f4discovery
+    stm32f103cb_als_mainboard_rev2
+    stm32f100cb_tempsensor
+    stm32f429zi_oledboard2
+    efm32gg332f1024_wandstem
+    stm32f411re_nucleo
+    stm32f429zi_skyward_anakin
+    stm32f100rc_solertegiard
+    stm32f205rc_skyward_stormtrooper
+    stm32f401vc_stm32f4discovery
+    stm32f103c8_breakout
+    stm32f100c8_microboard
+    stm32f469ni_stm32f469i-disco
+    stm32f429zi_skyward_homeone
+    stm32f429zi_skyward_rogallina
+    stm32f103c8_skyward_alderaan
+    stm32f401re_nucleo
+    stm32f746zg_nucleo
+    stm32h753xi_eval
+    stm32f407vg_thermal_test_chip
+    stm32f205_generic
+    stm32f103cx_generic
+    stm32f103cb_skyward_strain_board
+    stm32f072rb_stm32f0discovery
+    stm32f429zi_skyward_death_stack
+    stm32f429zi_skyward_death_stack_x
+    stm32f100cx_generic
+    stm32f407vg_skyward_tortellino
+    stm32f303vc_stm32f3discovery
+    stm32f100c8_vaisala_rs41
+    stm32l476rg_nucleo
+    atsam4lc2aa_generic
+)
