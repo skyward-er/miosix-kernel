a51a09651acb4fb7b3a46c17805b4cf5577693cc
Alberto Nidasio <alberto.nidasio@skywarder.eu>
2022-12-09 16:16:30 +0100

    [GroundStation] Added hwmappings for DIO1, DIO3

--- miosix/arch/cortexM4_stm32f4/stm32f429zi_skyward_groundstation_v2/interfaces-impl/hwmapping.h
+++ miosix/arch/cortexM4_stm32f4/stm32f429zi_skyward_groundstation_v2/interfaces-impl/hwmapping.h
@@ -30,61 +30,64 @@
 
 #include "interfaces/gpio.h"
 
-namespace miosix {
+namespace miosix
+{
 
-namespace interfaces 
+namespace interfaces
 {
 
 // CC3135
-namespace spi1 
+namespace spi1
 {
-using sck = Gpio<GPIOA_BASE, 5>;
+using sck  = Gpio<GPIOA_BASE, 5>;
 using miso = Gpio<GPIOB_BASE, 4>;
 using mosi = Gpio<GPIOA_BASE, 7>;
-} // namespace spi1
+}  // namespace spi1
 
 // RA-01
-namespace spi4 
+namespace spi4
 {
-using sck = Gpio<GPIOE_BASE, 2>;
+using sck  = Gpio<GPIOE_BASE, 2>;
 using miso = Gpio<GPIOE_BASE, 5>;
 using mosi = Gpio<GPIOE_BASE, 6>;
-} // namespace spi4
+}  // namespace spi4
 
-} // namespace interfaces
+}  // namespace interfaces
 
-namespace peripherals 
+namespace peripherals
 {
 
-namespace ra01 
+namespace ra01
 {
 
-namespace pc13 
+namespace pc13
 {
-using cs = Gpio<GPIOC_BASE, 13>;
+using cs   = Gpio<GPIOC_BASE, 13>;
 using dio0 = Gpio<GPIOF_BASE, 6>;
+using dio1 = Gpio<GPIOA_BASE, 2>;
+using dio3 = Gpio<GPIOC_BASE, 11>;
 using nrst = Gpio<GPIOC_BASE, 14>;
-} // namespace pc13
+}  // namespace pc13
 
-namespace pe4 
+namespace pe4
 {
-using cs = Gpio<GPIOE_BASE, 4>;
+using cs   = Gpio<GPIOE_BASE, 4>;
 using dio0 = Gpio<GPIOE_BASE, 3>;
 using nrst = Gpio<GPIOG_BASE, 2>;
-} // namespace pe4
+}  // namespace pe4
 
-} // namespace ra01
+}  // namespace ra01
 
-namespace cc3135 
+namespace cc3135
 {
-using cs = Gpio<GPIOD_BASE, 4>;
-using hib = Gpio<GPIOG_BASE, 3>;
+using cs   = Gpio<GPIOD_BASE, 4>;
+using hib  = Gpio<GPIOG_BASE, 3>;
 using intr = Gpio<GPIOD_BASE, 5>;
 using nrst = Gpio<GPIOB_BASE, 7>;
-} // namespace cc3135
+}  // namespace cc3135
 
-} // namespace peripherals
+}  // namespace peripherals
 
-} // namespace miosix
+}  // namespace miosix
 
-#endif // HWMAPPING_H
+#endif  // HWMAPPING_H
