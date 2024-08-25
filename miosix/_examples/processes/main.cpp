9685b46f5cbd0e492e3b357e5dcb2e5ff7e60882
Alberto Nidasio <nidasioalberto@gmail.com>
2024-08-25 18:55:59 +0200

    Improved processes example

--- miosix/_examples/processes/main.cpp
+++ miosix/_examples/processes/main.cpp
@@ -3,20 +3,28 @@
 
 #include <cstdio>
 
+void start_process(pid_t* pid, char* const argv[], char* const envp[])
+{
+    int error_code = posix_spawn(pid, argv[0], NULL, NULL, argv, envp);
+
+    if(error_code != 0)
+    {
+        printf("spawn returned %d\n", error_code);
+    }
+}
+
 int main()
 {
     pid_t pid1, pid2;
 
     // Arguments and environment variables
-    const char *arg1[] = {"/bin/process1", nullptr};
-    const char *arg2[] = {"/bin/process2", nullptr};
-    const char *env[]  = {nullptr};
+    const char* arg1[] = {"/bin/process1", nullptr};
+    const char* arg2[] = {"/bin/process2", nullptr};
+    const char* env[]  = {nullptr};
 
     // Start the processes
-    posix_spawn(&pid1, arg1[0], NULL, NULL, (char *const *)arg1,
-                (char *const *)env);
-    posix_spawn(&pid2, arg2[0], NULL, NULL, (char *const *)arg2,
-                (char *const *)env);
+    start_process(&pid1, (char* const*)arg1, (char* const*)env);
+    start_process(&pid2, (char* const*)arg2, (char* const*)env);
 
     // Wait for them to end
     wait(NULL);
